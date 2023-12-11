6f57710ed5543d392d174a85067ad2b3fb96019c
mainhy <79070181+mainhy@users.noreply.github.com>
2023-12-11 10:39:55 +0700

    Update basetting.php

--- assets/admin/fields/basetting.php
+++ assets/admin/fields/basetting.php
@@ -6,46 +6,53 @@
 **/
 defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
+use Joomla\CMS\Form\FormField;
+use Joomla\CMS\Form\FormHelper;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\Filesystem\Path;
+use Joomla\CMS\Filesystem\Folder;
+use Joomla\CMS\Filesystem\File;
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Router\Route;
 
-jimport('joomla.html.html');
-jimport('joomla.form.formfield');
-
-class JFormFieldBasetting extends JFormField
+class JFormFieldBasetting extends FormField
 {
-    public $type = 'Basetting';
+    protected $type = 'Basetting';
     public function getInput()
     {
-        require_once dirname(__FILE__).'/form-render.php';
-        JHtml::_('jquery.framework');
-        $baRender = new BestFormRender;
-        $doc = JFactory::getDocument();
-        $db = JFactory::getDbo();
+        require_once dirname(__FILE__) . '/form-render.php';
+        HTMLHelper::_('jquery.framework');
+        $baRender = new BestFormRender();
+        $doc = Factory::getDocument();
+        $db = Factory::getDbo();
         $modName = basename(dirname(dirname(dirname(__DIR__))));
-        $base_url = ((int)JVERSION >= 4 ? '' : JURI::root(true).'/').'modules/'.$modName.'/assets/admin';
+        $base_url = ((int)JVERSION >= 4 ? '' : Uri::root(true) . '/') . 'modules/' . $modName . '/assets/admin';
         if ((int)JVERSION >= 4) {
             $wa = $doc->getWebAssetManager();
-            $wa->registerAndUseScript('ba-jquery-ui', $base_url.'/js/jquery-ui-custom.min.js');
+            $wa->registerAndUseScript('ba-jquery-ui', $base_url . '/js/jquery-ui-custom.min.js');
             $wa->registerAndUseScript('ba-tinymce-js', 'media/vendor/tinymce/tinymce.min.js');
-            $wa->registerAndUseScript('ba-modadmin0-js', $base_url.'/js/color-picker.js');
-            $wa->registerAndUseScript('ba-modadmin1-js', $base_url.'/js/form.js');
-            $wa->registerAndUseScript('ba-modadmin2d-js', $base_url.'/js/admin.js');
-            $wa->registerAndUseStyle('ba-modadmin-css', $base_url.'/css/admin.css', [], [], ['fontawesome']);
-            $wa->addInlineScript('var modName="'.$modName.'";');
+            $wa->registerAndUseScript('ba-modadmin0-js', $base_url . '/js/color-picker.js');
+            $wa->registerAndUseScript('ba-modadmin1-js', $base_url . '/js/form.js');
+            $wa->registerAndUseScript('ba-modadmin2-js', $base_url . '/js/admin.js');
+            $wa->registerAndUseStyle('ba-modadmin-css', $base_url . '/css/admin.css', [], [], ['fontawesome']);
+            $wa->addInlineScript('var modName="' . $modName . '";');
         } else {
             $doc->addStyleSheet('//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css');
-            $doc->addScript(JURI::root(true).'/media/editors/tinymce/tinymce.min.js');
-            $doc->addScript($base_url.'/js/jquery-ui-custom.min.js');
-            $doc->addScript($base_url.'/js/color-picker.js');
-            $doc->addScript($base_url.'/js/form.js');
-            $doc->addScript($base_url.'/js/admin.js');
-            $doc->addStyleSheet($base_url.'/css/admin.css');
-            $doc->addScriptDeclaration('var systemPathRoot="'.JURI::root(true).'";var modID='.JFactory::getApplication()->input->get('id').'; var modName="'.$modName.'";function jModalClose(){return false};function jInsertFieldValue(value, id){return false};'); // SET Module ID & Name for AJAX via JS
+            $doc->addScript(Uri::root(true) . '/media/editors/tinymce/tinymce.min.js');
+            $doc->addScript($base_url . '/js/jquery-ui-custom.min.js');
+            $doc->addScript($base_url . '/js/color-picker.js');
+            $doc->addScript($base_url . '/js/form.js');
+            $doc->addScript($base_url . '/js/admin.js');
+            $doc->addStyleSheet($base_url . '/css/admin.css');
+            $doc->addScriptDeclaration('var systemPathRoot="' . Uri::root(true) . '";var modID=' . Factory::getApplication()->input->get('id') . '; var modName="' . $modName . '";function jModalClose(){return false};function jInsertFieldValue(value, id){return false};'); // SET Module ID & Name for AJAX via JS
         }
-        
+
+
         $getVal = $this->form->getValue('content');
         if (empty($getVal)) {
             $db->setQuery('ALTER TABLE #__modules MODIFY content LONGTEXT')->execute();
-            JFile::delete(JPATH_ADMINISTRATOR.'/components/com_admin/sql/updates/mysql/4.0.0-2018-03-05.sql');
+            File::delete(JPATH_ADMINISTRATOR . '/components/com_admin/sql/updates/mysql/4.0.0-2018-03-05.sql');
         }
         $getVal = empty($getVal) ? $baRender->defalutData() : $getVal;
         $baData = json_decode($getVal, true);
